19683525288656a5c8201dd904cb59c824edef7a
Austin <30707585+Perfectdotexe@users.noreply.github.com>
2019-06-21 08:51:35 -0600

    Update game.py

--- game.py
+++ game.py
@@ -2,13 +2,13 @@
 
 import random # Import random library for game to function.
 
-## Actual program
+# Actual program
 print('Lets play a game of shell game, choose any cup and if the cup has the ball underneath it you win. There is ten cups and you are blindfolded.')
 print('Choose a number through 1-10, this will be your choice for the cup in left to right chronological order.')
 userInput = input()
 ballPlaceholder = random.randrange(1,10)
 
-## Decision making process based on values.
+# Decision making process based on values.
 if int(userInput) == ballPlaceholder:
     print('You have won!')
 elif int(userInput) < ballPlaceholder:
